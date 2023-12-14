76c90afcf62de897f2e8958cfd0dd4115e1c9d71
Taras <topanasenko85@gmail.com>
2023-12-14 12:02:17 +0200

    ex9

--- ex9/app.js
+++ ex9/app.js
@@ -2,14 +2,49 @@ require('dotenv').config()
 
 const express =require('express')
 const app =express()
+const articles = [{title: 'exemple'}, {title: 'exemple2'}, {title: 'exemple3'}]
+const bodyParser = require('body-parser')
 
-const port = process.env.PORT || 3001
+app.set('port', process.env.PORT || 3001)
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}))
 
 
-app.get('/', (req, res)=>{
-    res.send('H World')
+app.get('/articles', (req, res, next)=>{
+    res.send(articles)
 })
 
-app.listen(port,()=>{
-    console.log(`Web app available at http://127.0.0.1:${port}`)
+app.post('/articles', (req, res, next)=>{
+    article = {title: req.body.title}
+    articles.push(article)
+    res.send(article)
 })
+
+app.get('/articles/:id', (req, res, next)=>{
+    const id = req.params.id
+    console.log('frtching:',id)
+    res.send (articles[id])
+})
+
+app.delete('/articles/:id', (req, res, next)=>{
+    const id = req.params.id
+    console.log('deleting:',id)
+    delete articles[id]
+    res.send ({message: 'DELETED'})
+})
+
+
+app.listen(app.get('port'),()=>{
+    console.log(`Web app available at http://127.0.0.1:${app.get('port')}`)
+})
+
+module.exports = app
+
+
+
+// POST/articles
+// GET/articles/:id
+// GET/articles
+// DELETE/articles/:id
+
